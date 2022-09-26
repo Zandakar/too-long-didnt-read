2bc69c58ce7588b4ba512f9d4c8a0fdce8a90df9
root <root@DESKTOP-373MK7L.localdomain>
2022-09-26 18:59:38 +0200

    initial commit

---
+++ src/utils/AppConfig.ts
@@ -0,0 +1,10 @@
+export const AppConfig = {
+  site_name: 'Starter',
+  title: `Too Long; Didn't Read`,
+  description:
+    `A blog about travel`,
+  url: 'https://example.com',
+  locale: 'en',
+  author: 'Anonymous',
+  pagination_size: 5,
+};
