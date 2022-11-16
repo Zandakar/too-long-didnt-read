e4f43420efa3ea37c96784cc32c99c5f5b48fe85
Zandakar <34891041+Zandakar@users.noreply.github.com>
2022-11-17 00:11:45 +0100

    added tracking (#9)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,33 @@
 import React from 'react';
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import ReactGA from 'react-ga';
 
 import { AppProps } from 'next/app';
 
 import '../styles/main.css';
 import '../styles/prism-a11y-dark.css';
 
-const MyApp = ({ Component, pageProps }: AppProps) => (
-  <Component {...pageProps} />
-);
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = () => {
+    ReactGA.set({ page: location.pathname });
+    ReactGA.pageview(location.pathname)
+    }
+
+    router.events.on('routeChangeStart', handleRouteChange)
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method:
+    return () => {
+      router.events.off('routeChangeStart', handleRouteChange)
+    }
+  }, [])
+  
+  return <Component {...pageProps} />
+};
+
 
 export default MyApp;
