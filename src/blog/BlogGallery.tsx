6d37dcddb843b9d8c1126716a4762d77262ede02
Tom de Ruijter <tom.deruijter@hotmail.com>
2022-11-19 00:17:11 +0100

    trying something

--- src/blog/BlogGallery.tsx
+++ src/blog/BlogGallery.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
 import { format } from 'date-fns';
+import { useRouter } from 'next/router'
+
 import Link from 'next/link';
 
 import { Pagination, IPaginationProps } from '../pagination/Pagination';
@@ -11,12 +13,35 @@ export type IBlogGalleryProps = {
   pagination: IPaginationProps;
 };
 
+
+
 const BlogGallery = (props: IBlogGalleryProps) => (
   <>
     <ul>
-      {props.posts.map((elt) => (
-        <li key={elt.slug} className="mb-3 flex justify-between">
-          <Link href="/posts/[slug]" as={`/posts/${elt.slug}`}>
+      {props.posts.map((elt) => {
+        // Get full URL (localhost or too-long...)
+        const { asPath } = useRouter();
+        const origin =
+          typeof window !== 'undefined' && window.location.origin
+            ? window.location.origin
+            : '';
+
+        const URL = `${origin}${asPath}`;
+        // console.log(URL)
+        const isDevelopmentServer = URL.includes("localhost")
+        // if (isDevelopmentServer) {
+        //   console.log("@@@@@@@@@@@@@@@@@@")
+        // }
+
+        const indexPostpend = !isDevelopmentServer ? `/index.html` : ''
+
+        console.log(indexPostpend)
+        console.log(`/posts/${elt.slug} + ${indexPostpend}`)
+
+
+        return (<li key={elt.slug} className="mb-3 flex justify-between">
+          <Link href={`/posts/[slug]${indexPostpend}`}
+            as={`/posts/${elt.slug}${indexPostpend}`}>
             <a>
               <h2>{elt.title}</h2>
             </a>
@@ -25,8 +50,8 @@ const BlogGallery = (props: IBlogGalleryProps) => (
           <div className="text-right">
             {format(new Date(elt.date), 'LLL d, yyyy')}
           </div>
-        </li>
-      ))}
+        </li>)
+      })}
     </ul>
 
     <Pagination
--- src/pages/resources.tsx
+++ src/pages/resources.tsx
@@ -13,14 +13,14 @@ const Page = () => (
   >
     <Content>
       <h1 className="text-center  text-3xl text-gray-900">{`-- Resources --`}</h1>
-      <div>       <a href="https://too-long-didnt-read.au/posts/post-04/">My Gear</a>{`- My initial packing list for Europe from Sept to Nov`}</div>
+      <div>       <a href="https://too-long-didnt-read.au/posts/post-04/index.html">My Gear</a>{`- My initial packing list for Europe from Sept to Nov`}</div>
       <br></br>
       <h2 className="text-center  text-2xl text-gray-900">{`General cool things`}</h2>
       <br></br>
       <div>       <a href="https://www.youtube.com/watch?v=_Qqp62SWWXM&ab_channel=MARATONNIETRAVELS">Same Same But Different </a>{`- One of the best 
       documentaries about backpacking I've come across`}</div>
       <br></br>
-            <div>       <a href="https://www.goodreads.com/book/show/100247.Vagabonding"> Vagabonding, the Art of Long Term Travel</a>{`- Great book about
+      <div>       <a href="https://www.goodreads.com/book/show/100247.Vagabonding"> Vagabonding, the Art of Long Term Travel</a>{`- Great book about
             how to approach backpacking. The mentality, how to get started etc. Short read`}</div>
       <br></br>
       <h2 className="text-center  text-2xl text-gray-900">{`-- Useful Apps --`}</h2>
@@ -29,23 +29,23 @@ const Page = () => (
       <div className="flex justify-center items-center"></div>
       <div>
         <ol className="list-decimal">
-                   <li>{`HostelWorld - Best app for booking hostels. Puts you in chats with all others who are staying there`}</li>
-                   <li>{`AirBnB - Other than accomidation, the experiences you can find on here are pretty cool`}</li>
-                   <li>{`GetYourGuide - Walking tours, bar crawls, general experiences`}</li>
-                   <li>{`GuruWalk - Cheap and consistently good walking tours. Great first activity in a city`}</li>
-                  <li>{`Workaway - find volunteer work. Costs 50€ / year but has some really cool gigs`}</li>
-                  <li>{`FlixBus - Cheap European intra and international travel`}</li>
+          <li>{`HostelWorld - Best app for booking hostels. Puts you in chats with all others who are staying there`}</li>
+          <li>{`AirBnB - Other than accomidation, the experiences you can find on here are pretty cool`}</li>
+          <li>{`GetYourGuide - Walking tours, bar crawls, general experiences`}</li>
+          <li>{`GuruWalk - Cheap and consistently good walking tours. Great first activity in a city`}</li>
+          <li>{`Workaway - find volunteer work. Costs 50€ / year but has some really cool gigs`}</li>
+          <li>{`FlixBus - Cheap European intra and international travel`}</li>
         </ol>
         <br />
       </div>
-            <h2 className="text-center  text-2xl text-gray-900">{`Other`}</h2>
-            <br></br>
+      <h2 className="text-center  text-2xl text-gray-900">{`Other`}</h2>
+      <br></br>
       <div className="flex justify-center items-center"></div>
       <div>
         <ol className="list-decimal">
-                   <li>{`NSRail (The Netherlands) - Google maps can't be trusted in NL. Use this instead if you have to, although simply
+          <li>{`NSRail (The Netherlands) - Google maps can't be trusted in NL. Use this instead if you have to, although simply
                    rocking up to a train station and waiting ~20 mins never failed me`}</li>
-                   <li>{`DonkeyRepublic (The Netherlands / Germany) - Bike sharing app. Works best in major cities`}</li>
+          <li>{`DonkeyRepublic (The Netherlands / Germany) - Bike sharing app. Works best in major cities`}</li>
         </ol>
         <br />
       </div>
