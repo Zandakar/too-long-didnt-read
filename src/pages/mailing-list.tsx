42e0d2d87ef0d5aaca273b639ce6cc701a5e6534
Tom de Ruijter <tom.deruijter@hotmail.com>
2022-11-23 15:53:40 +0100

    working proper email input

--- src/pages/mailing-list.tsx
+++ src/pages/mailing-list.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { Content } from "../content/Content";
 import { Meta } from "../layout/Meta";
@@ -10,47 +10,58 @@ import fetch from 'node-fetch';
 
 // https://mui.com/material-ui/api/form-control/
 
-const onClick = async () => {
-  const body = { "email": "fromWebsite@test.com" };
-  const API_GATEWAY_URL = `https://p23xchxvcg.execute-api.ap-southeast-2.amazonaws.com/prd/email/add`
+const MailingList = () => {
+  const [emailField, setEmailFild] = useState()
 
-  try {
-    const response = await fetch(API_GATEWAY_URL, {
-      method: 'post',
-      body: JSON.stringify(body),
-      headers: { 'Content-Type': 'application/json' }
-    });
-    const data = await response.json();
+  const onClick = async () => {
+    console.log(emailField)
+    const body = { "email": emailField };
+    const API_GATEWAY_URL = `https://p23xchxvcg.execute-api.ap-southeast-2.amazonaws.com/prd/email/add`
 
-    console.log(data)
-  } catch (e) {
-    console.error(e)
+    try {
+      const response = await fetch(API_GATEWAY_URL, {
+        method: 'post',
+        body: JSON.stringify(body),
+        headers: { 'Content-Type': 'application/json' }
+      });
+      const data = await response.json();
+
+      console.log(data)
+    } catch (e) {
+      console.error(e)
+    }
+
   }
 
-}
+
+  const bla = ((e) => {
+    console.log(e.target.value)
+    setEmailFild(e.target.value)
+  })
 
 
-const MailingList = () => (
-  <Main meta={<Meta title="Mailing List" description="Mailing List" />}>
-    < Content >
-      <h1 className="text-center  text-3xl text-gray-900">{`Mailing List`}</h1>
+  return (
+    <Main meta={<Meta title="Mailing List" description="Mailing List" />}>
+      < Content >
+        <h1 className="text-center  text-3xl text-gray-900">{`Mailing List`}</h1>
+        <br></br>
+        <div className="flex justify-center items-center">
+          <div>If you would like a friendly reminder whenever I add a new post, add your email below</div>
+        </div>
+        <br></br>
+        <TextField id="filled-basic" label="Email Address" variant="filled" onChange={bla} />
+      </Content >
       <br></br>
-      <div className="flex justify-center items-center">
-        <div>If you would like a friendly reminder whenever I add a new post, add your email below</div>
-      </div>
-      <br></br>
-      <TextField id="filled-basic" label="Email Address" variant="filled" />
-    </Content >
-    <br></br>
-    <Button
-      onClick={onClick}
-      style={{
-        borderRadius: 35,
-        backgroundColor: "#419bf0",
-      }} variant="contained" endIcon={<SendIcon />}>
-      Add me!
-    </Button>
-  </Main >
-);
+      <Button
+        onClick={onClick}
+        style={{
+          borderRadius: 35,
+          backgroundColor: "#419bf0",
+        }} variant="contained" endIcon={<SendIcon />}>
+        Add me!
+      </Button>
+    </Main >
+  )
+};
 
 export default MailingList;
