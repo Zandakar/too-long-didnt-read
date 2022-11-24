73bb2fbc454a0df43939ef4ed46b73497617ed8d
Tom de Ruijter <tom.deruijter@hotmail.com>
2022-11-24 22:13:02 +0100

    cleanup

--- src/pages/mailing-list.tsx
+++ src/pages/mailing-list.tsx
@@ -31,8 +31,7 @@ const MailingList = () => {
         const response = await fetch(API_GATEWAY_URL, {
           method: 'post',
           body: JSON.stringify(body),
-          headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' },
-          mode: 'cors'
+          headers: { 'Content-Type': 'application/json' },
         });
         const data = await response.json();
 
