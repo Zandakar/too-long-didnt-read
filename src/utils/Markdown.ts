c77161c56eab810b22a71f45f9f057842ef92b79
Tom de Ruijter <tom.deruijter@hotmail.com>
2022-11-25 21:56:32 +0100

    added lazy loader package

--- src/utils/Markdown.ts
+++ src/utils/Markdown.ts
@@ -1,9 +1,9 @@
-// TODO: @mapbox/rehype-prism does not have typescript definition
 // @ts-ignore
 import rehypePrism from '@mapbox/rehype-prism';
 import html from 'rehype-stringify';
 import gfm from 'remark-gfm';
 import remarkParse from 'remark-parse';
+import lazyLoadPlugin from 'rehype-plugin-image-native-lazy-loading'
 import remarkRehype from 'remark-rehype';
 import { unified } from 'unified';
 
@@ -13,11 +13,15 @@ export async function markdownToHtml(markdown: string) {
     .use(gfm)
     .use(remarkRehype)
     .use(rehypePrism)
+    // @ts-ignore
+    .use(lazyLoadPlugin)
     .use(html)
     .process(markdown);
 
 
-  const replacedResult = result.toString().replace(/@@baseUrl@@/g, process.env.baseUrl || '').replaceAll(`<img src=`, `<img loading="lazy" src=`);
+  const replacedResult = result.toString().replace(/@@baseUrl@@/g, process.env.baseUrl || '')
+
+  console.log(replacedResult)
   return replacedResult
 
 
