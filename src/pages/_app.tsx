21dfdad91df55282a845e389d127288cc27a0558
Tom de Ruijter <tom.deruijter@hotmail.com>
2022-12-18 00:46:52 +0100

    fix-analytics-again

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
-import { useEffect } from 'react'
-import { useRouter } from 'next/router'
-import ReactGA from 'react-ga';
-// import { GoogleAnalytics } from "nextjs-google-analytics";
+
 
 import { AppProps } from 'next/app';
 
@@ -10,27 +7,6 @@ import '../styles/main.css';
 import '../styles/prism-a11y-dark.css';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  const router = useRouter()
-
-  ReactGA.initialize('G-5LX7ZPWY7X');
-  ReactGA.pageview('/');
-  console.log("Get out of my console you nerd")
-
-  useEffect(() => {
-    const handleRouteChange = () => {
-      ReactGA.set({ page: location.pathname });
-      ReactGA.pageview(location.pathname)
-    }
-
-    router.events.on('routeChangeComplete', handleRouteChange)
-
-    // If the component is unmounted, unsubscribe
-    // from the event with the `off` method:
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange)
-    }
-  }, [])
-
   return <Component {...pageProps} />
 };
 
