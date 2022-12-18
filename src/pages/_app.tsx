688f6aa261c74130e8838e190ba1bca9a4e96ce6
Zandakar <34891041+Zandakar@users.noreply.github.com>
2022-12-18 22:43:35 +0100

    Added script to app.tsx (#25)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,34 @@
 import React from 'react';
-
-
+import Script from 'next/script';
 import { AppProps } from 'next/app';
 
 import '../styles/main.css';
 import '../styles/prism-a11y-dark.css';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-5LX7ZPWY7X"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-5LX7ZPWY7X');
+        `}
+      </Script>
+
+      <Component {...pageProps} />
+    </>
+  );
 };
 
 
 export default MyApp;
+
+
+
